61853eca2545218e4805121456ccd2b3496c24de
Eric Giovannetti <giovannetti.eric@gmail.com>
2015-11-21 22:07:04 -0500

    Change default bash

--- brew.sh
+++ brew.sh
@@ -26,11 +26,11 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+chsh -s /usr/local/bin/bash
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
